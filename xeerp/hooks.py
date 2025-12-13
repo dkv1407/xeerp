5a3a900b9ccacc928b2c788bf2e76e2881c1d031
dkv1407 <diwakar@xenonlabs.ai>
2025-12-14 02:05:32 +0300

    Fix: Permission Issue

--- xeerp/hooks.py
+++ xeerp/hooks.py
@@ -11,15 +11,15 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "xeerp",
-# 		"logo": "/assets/xeerp/xeERP-Fevicon.png",
-# 		"title": "XeERP",
-# 		"route": "/app/xeerp",
-# 		"has_permission": "xeerp.api.permission.has_app_permission"
-# 	}
-# ]
+add_to_apps_screen = [
+	{
+		"name": "xeerp",
+		"logo": "/assets/xeerp/xeERP-Fevicon.png",
+		"title": "XeERP",
+		"route": "/app/xeerp",
+		# "has_permission": "xeerp.api.permission.has_app_permission"
+	}
+]
 
 # Includes in <head>
 # ------------------
